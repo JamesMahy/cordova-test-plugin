1084252c086b67b3670169e924b11d6d973a5481
James Mahy <james.mahy@cevo.co.uk>
2019-06-14 13:23:06 +0100

    Finished working plugin

--- src/ios/TestPlugin.m
+++ src/ios/TestPlugin.m
@@ -3,28 +3,49 @@
 
 @implementation TestPlugin
 
-    -(void)show:(CDVInvokedUrlCommand*)command
-    {
-        CDVPluginResult* pluginResult = nil;
-        NSString* message = [command.arguments objectAtIndex:0];
-        NSInteger* duration = [command.arguments objectAtIndex:1];
+-(void)show:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    
+    NSDictionary* arguments = [command.arguments objectAtIndex:0];
+    
+    
+    NSString* message = [arguments valueForKey:@"message"];
+    NSNumber* duration = [arguments valueForKey:@"duration"];
+    
+    if(message == nil || [message length] <= 0){
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }else{
         
-        if(message = nil || [message length] <= 0){
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-        }else{
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Title"
-                                                            message:@"Message"
-                                                           delegate:nil
-                                                  cancelButtonTitle:@"OK"
-                                                  otherButtonTitles:nil];
-            [alert show];
-            [alert release];
-            
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@""
+                                     message: message
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        
+        
+        UIAlertAction* okButton = [UIAlertAction
+                                   actionWithTitle:@"OK"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {
+                                       //Handle your yes please button action here
+                                   }];
+        
+        [alert addAction:okButton];
+        
+        [self.viewController presentViewController:alert animated:YES completion:nil];
+        
+        if(duration > 0){
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, [duration integerValue] * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                [alert dismissViewControllerAnimated:true completion:nil];
+            });
         }
         
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     }
     
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
 
 @end
